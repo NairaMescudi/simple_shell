1222702c6e38e509d7f574a4f82287bb029a7756
NairaMescudi <tdaisioso@gmail.com>
2023-12-11 03:25:11 +0100

    Added a function to get the command path

--- shell.h
+++ shell.h
@@ -41,6 +41,6 @@ int handleAliasCommands(alias_t *aliasList, char *argument);
 char **tokenizer(char *line, const char *delim);
 int executeCommand(char **tokens, char **argv, size_t cmd_count);
 extern char** environ;
-
+void get_path(char **pathname);
 
 #endif
